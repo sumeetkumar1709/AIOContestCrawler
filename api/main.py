c4703917eb959efc406d1b031eedb051b582da22
Sumeet Kumar <sumeetkumar1709@gmail.com>
2024-10-16 23:07:10 +0530

    add scheduler + supabase db

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI, HTTPException, Header
+from apscheduler.schedulers.background import BackgroundScheduler  # Import BackgroundScheduler
 
 from crawlers import ContestCrawler
 
@@ -14,6 +15,26 @@ with open('files/platforms.json', 'r') as f:
 
 API_TOKEN = os.getenv('API_TOKEN')
 
+# Initialize the scheduler
+scheduler = BackgroundScheduler()
+
+def scheduled_task():
+    platform_list = list(platforms.keys())
+    for plat in platform_list:
+        ContestCrawler(plat,'crawl')
+    print("Scheduled task executed") 
+
+# Schedule the task to run every hour
+scheduler.add_job(scheduled_task, 'interval', hours = 1)
+
+def start_scheduler():
+    scheduler.start()  
+
+def shutdown_scheduler():
+    scheduler.shutdown() 
+
+app.add_event_handler("startup", start_scheduler)  # Register the startup event handler
+app.add_event_handler("shutdown", shutdown_scheduler)  # Register the shutdown event handler
 
 @app.get("/platforms/{platform_name}")
 async def get_platform_info(platform_name: str, x_api_token: str = Header(...)):
@@ -24,9 +45,14 @@ async def get_platform_info(platform_name: str, x_api_token: str = Header(...)):
 
         platform_info = platforms.get(platform_name)
         if platform_info and  'ContestCrawler' in platform_info['crawlers']:
-            return ContestCrawler(platform_name)
+            return ContestCrawler(platform_name,'fetch')
         raise HTTPException(status_code=404, detail="Platform not found")
     
     except Exception as e:
         print(f"Internal server error: {e}")
         raise HTTPException(status_code=500, detail="Internal server error")
+
+@app.get("/force-run-task")
+async def force_run_task():
+    scheduled_task()  
+    return {"message": "Scheduled task executed immediately."}
